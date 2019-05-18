2cb6a932b7a3a04a563d94ac0996afb97b62d37a
LiamH <liamhearts@gmail.com>
2019-05-18 14:14:31 -0700

    added liri to portfolio

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$("#linkedin").on("click",function(){
+    window.open('https://www.linkedin.com/in/liam-hartigan-581ab9176/', 'linkedin'); 
+})
+
+$("#github").on("click",function(){
+    window.open('https://github.com/VanillaCoder', 'github'); 
+})
+
+
